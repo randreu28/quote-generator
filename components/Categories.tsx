b2a4846cb0c35f6e630924c070b91f201f448ed5
Rubén Chiquin <randreu28@gmail.com>
2023-06-15 19:59:27 +0200

    Makes the <Categories/> responsive

--- components/Categories.tsx
+++ components/Categories.tsx
@@ -3,7 +3,7 @@ export default function Categories() {
   const categories = ["any", "age", "alone", "amazing", "anger", "architecture", "art", "attitude", "beauty", "best", "birthday", "business", "car", "change", "communications", "computers", "cool", "courage", "dad", "dating", "death", "design", "dreams", "education", "environmental", "equality", "experience", "failure", "faith", "family", "famous", "fear", "fitness", "food", "forgiveness", "freedom", "friendship", "funny", "future", "god", "good", "government", "graduation", "great", "happiness", "health", "history", "home", "hope", "humor", "imagination", "inspirational", "intelligence", "jealousy", "knowledge", "leadership", "learning", "legal", "life", "love", "marriage", "medical", "men", "mom", "money", "morning", "movies", "success"];
 
   return (
-    <div class="grid grid-cols-8">
+    <div class="grid grid-cols-2 text-center lg:grid-cols-8">
       {categories.map((category) => {
         return (
           <a
